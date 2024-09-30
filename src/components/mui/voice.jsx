8f444fee41c991fd110613c9550ddd4713139681
kate0626 <101503385+Maikooon@users.noreply.github.com>
2024-09-30 12:05:28 +0900

    modify voice

--- src/components/mui/voice.jsx
+++ src/components/mui/voice.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import { React,useState } from 'react';
 import './../../assets/styles/voiceCard.css';
 import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
+import ',/../assets/styles/test.css';
 
-const VoiceCard = ({title,company,belong,text}) => {
-    const [value, setValue] = React.useState(2);
+const VoiceCard = ({company,belong,rank, text}) => {
+    // const [value, setValue] = useState();
+
     //多分ここでカードに入る情報を取得する
     return (
         <>
@@ -16,7 +17,7 @@ const VoiceCard = ({title,company,belong,text}) => {
                 <div class="university-info">{belong}</div>
                 <div class="rating">
                     <span>満足度</span>
-                    <Rating name="read-only" value={value} readOnly />
+                    <Rating name="read-only" value={rank} readOnly />
                 </div>
                 <div class="comment">
                     {text}
@@ -27,26 +28,51 @@ const VoiceCard = ({title,company,belong,text}) => {
 }
 
 const Voice = () => {
+    const data = [
+        {
+            company:"楽天グループ株式会社に内定",
+            belong:"東京大学 経済部3年",
+            rank:5,
+            text:"検索機能の使いやすさがよかったです。最初は就活に役立つアルバイト代わりになれば、という思いで探し始めたが、本気で成長させたいと思わせてくれるきっかけになりました。"
+        },
+        {
+            company:"楽天グループ株式会社に内定",
+            belong:"東京大学 経済部3年",
+            rank:5,
+            text:"検索機能の使いやすさがよかったです。最初は就活に役立つアルバイト代わりになれば、という思いで探し始めたが、本気で成長させたいと思わせてくれるきっかけになりました。"
+        },
+        {
+            company:"楽天グループ株式会社に内定",
+            belong:"東京大学 経済部3年",
+            rank:5,
+            text:"検索機能の使いやすさがよかったです。最初は就活に役立つアルバイト代わりになれば、という思いで探し始めたが、本気で成長させたいと思わせてくれるきっかけになりました。"
+        },
+        {
+            company:"楽天グループ株式会社に内定",
+            belong:"東京大学 経済部3年",
+            rank:5,
+            text:"検索機能の使いやすさがよかったです。最初は就活に役立つアルバイト代わりになれば、という思いで探し始めたが、本気で成長させたいと思わせてくれるきっかけになりました。"
+        },
+        {
+            company:"楽天グループ株式会社に内定",
+            belong:"東京大学 経済部3年",
+            rank:5,
+            text:"検索機能の使いやすさがよかったです。最初は就活に役立つアルバイト代わりになれば、という思いで探し始めたが、本気で成長させたいと思わせてくれるきっかけになりました。"
+        },
+    ];
     return (
         <>
             <div class="voice-container">
-                <VoiceCard 
-                    company = "楽天グループ株式会社に内定" 
-                    belong = "東京大学 経済部3年" 
-                    text = "検索機能の使いやすさがよかったです。最初は就活に役立つアルバイト代わりになれば、という思いで探し始めたが、本気で成長させたいと思わせてくれるきっかけになりました。"/>
-                <VoiceCard 
-                        company = "楽天グループ株式会社に内定" 
-                        belong = "東京大学 経済部3年" 
-                        text = "検索機能の使いやすさがよかったです。最初は就活に役立つアルバイト代わりになれば、という思いで探し始めたが、本気で成長させたいと思わせてくれるきっかけになりました。"/>
-                <VoiceCard 
-                        company = "楽天グループ株式会社に内定" 
-                        belong = "東京大学 経済部3年" 
-                        text = "検索機能の使いやすさがよかったです。最初は就活に役立つアルバイト代わりになれば、という思いで探し始めたが、本気で成長させたいと思わせてくれるきっかけになりました。"/>
-                <VoiceCard 
-                        company = "楽天グループ株式会社に内定" 
-                        belong = "東京大学 経済部3年" 
-                        text = "検索機能の使いやすさがよかったです。最初は就活に役立つアルバイト代わりになれば、という思いで探し始めたが、本気で成長させたいと思わせてくれるきっかけになりました。"/>
-            
+                
+                {data.map((data) =>
+                    <VoiceCard  
+                        company={data.company} 
+                        belong={data.belong} 
+                        rank={data.rank}    
+                        text={data.text}/>
+                    )
+                }
+                
             </div>
         </>
     )
