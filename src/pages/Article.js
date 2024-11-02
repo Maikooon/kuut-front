4a8b829f3677f49789971815c7568a7acbbdff72
kate0626 <101503385+Maikooon@users.noreply.github.com>
2024-11-02 13:19:49 +0900

    記事のPage完了

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -3,47 +3,46 @@ import Headers from "../components/layout/header";
 import './../assets/styles/article/article.css';
 import Footer from '../components/layout/footer';
 import './../assets/styles/article/articleCard.css';   
-import ArticleImage from './..//assets/images/article/image.png';
+import WOLF from './../assets/images/wolf.png';
 
-const ArticleCard = () => {
+const Tag = ({ explain }) => {
     return (
-        <div class="article-card">
-            <div class="article-card-img">
-
-                {/* <img src={ArticleImage} alt="article-image"  /> */}
+        <>
+            <div class="tag">
+                <p>
+                    {explain}
+                </p>
             </div>
-            <div class="article-card-text">
-                <div class="article-card-text-title">
-                    <h3>長期インターンのメリット</h3>
-                    <p>三菱商事＋三井物産＋外資戦コンという就活トップレベルの企業から内定を
-                    貰った東京大学修士2年生に、総合商社の就活対策を徹底インタビュー。商…</p>
-                    <p>2021年10月10日</p>
-                </div>
-            </div>
-        </div>
+        </>
     );
 }
 
-const Card = ({ title, subtitle, image, description, date }) => {
-  return (
-    <div className="card">
-      <div className="card-image">
-        <img src={image} alt={title} />
-      </div>
-      <div className="card-content">
-        <h3 className="card-title">{title}</h3>
-        <p className="card-subtitle">{subtitle}</p>
-        <p className="card-description">{description}</p>
-        <p className="card-date">{date}</p>
-      </div>
-    </div>
-  );
-};
-
-
+const ArticleCard = ({ explain }) => {
+    return (
+        <>
+            <div class="article-card">
+                <div class="image">
+                    <img src={WOLF} alt="company-image" />
+                </div>
+                <div class="text-box">
+                    <div class="tag-row">
+                        <Tag explain="株式会社Wolf Group"/>
+                        <Tag explain="株式会社Wolf Group"/>
+                        <Tag explain="株式会社Wolf Group"/>
+                    </div>
+                    
+                    <div class="title">月収100万円も目指せる広告代理店インターン</div>
+                    <div class="explain"> 三菱商事＋三井物産＋外資戦コンという就活トップレベルの企業から内定を
+                    貰った東京大学修士2年生に、総合商社の就活対策を徹底インタビュー。商…
+                    三菱商事＋三井物産＋外資戦コンという就活トップレベルの企業から内定</div>
+                    <div class="date">2021年10月10日</div>
+                </div>
+            </div>
+        </>
+    );
+}
 
 const Article = () => {
-
     return (
         <html>
             <header>
@@ -51,13 +50,13 @@ const Article = () => {
             </header>
             <body>
                 <div class = "article">
-                    <h1>長期インターンに関する記事一覧</h1>
-                    {/* ここから一列にカードを並べる */}
-                    <ArticleCard />
-                    <ArticleCard />
-                  
+                        <h1>長期インターンに関する記事一覧</h1>
+                        {/* ここから一列にカードを並べる */}
+                        <ArticleCard />
+                        <ArticleCard />
+                        <ArticleCard />
                 </div>
-                
+            
             </body>
             <footer>
                 <Footer/>
@@ -67,3 +66,4 @@ const Article = () => {
 }
 
 export default Article;
+
